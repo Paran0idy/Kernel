baca931c6aee481e899345a3e0169dab55063aeb
Jiaxing Ding <dingjiaxing.99@bytedance.com>
2024-08-30 16:23:49 +0800

    feat(sgemv): add warpReduce kernel

--- lib/sgemv.cu
+++ lib/sgemv.cu
@@ -14,6 +14,31 @@ void naive(float *a, float *b, float *c, int N, int K) {
     naive_kernel<<<N / BLOCK_SIZE, BLOCK_SIZE>>>(a, b, c, N, K);
 }
 
+__device__ int warpReduce(int val) {
+    for (int i = 16; i >= 1; i /= 2)
+        val += __shfl_xor_sync(0xffffffff, val, i);
+    return val;
+}
+
+__global__ void warpReduce_kernel(float *a, float *b, float *c, int N, int K) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int warpId = tid / 32;
+    int laneId = tid % 32;
+    int warpSize = 32;
+
+    int val = 0;
+    for (int i = 0; i < K; i += warpSize) 
+        val += a[laneId + i] * b[OFFSET(laneId + i, warpId, N)];
+    c[warpId] = warpReduce(val);
+}
+
+void warpReduce(float *a, float *b, float *c, int N, int K) {
+    const int NUM_THREAD = 1024;
+    const int WARP_SIZE = 32;
+    
+    warpReduce_kernel<<<N / (NUM_THREAD / WARP_SIZE), NUM_THREAD>>>(a, b, c, N, K);
+}
+
 int main() {
     int N = 64, K = 64;
 
@@ -36,7 +61,8 @@ int main() {
     cudaMemcpy(da, a, size_a, cudaMemcpyHostToDevice);
     cudaMemcpy(db, b, size_b, cudaMemcpyHostToDevice);
 
-    naive(da, db, dc, N, K);
+    // naive(da, db, dc, N, K);
+    warpReduce(da, db, dc, N, K);
 
     cudaMemcpy(c, dc, size_c, cudaMemcpyDeviceToHost);
 
