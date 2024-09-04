8e2cdcc6066563dbdf07b14535ec721366158124
Jesper Ding <dingjx@wanyantech.com>
2024-09-04 08:53:54 +0000

    feat(cute): refactor cute gemm-simple

--- lib/cute/gemm-simple.cu
+++ lib/cute/gemm-simple.cu
@@ -2,16 +2,14 @@
 #include <stdlib.h>
 #include "util.h"
 
-// #define PRINT_INFO
+#define PRINT_INFO
 using namespace cute;
 
 template <typename T>
 void gen_rand_data(T *data, int n);
 
 template <typename T, int kTileM, int kTileN, int kTileK, typename TiledMMA>
-__global__ void gemm_simple(T *Cptr, const T *Aptr, const T *Bptr, int m, int n, int k)
-{
-
+__global__ void gemm_simple(T *Cptr, const T *Aptr, const T *Bptr, int m, int n, int k) {
   Tensor A = make_tensor(make_gmem_ptr(Aptr), make_shape(m, k), make_stride(k, Int<1>{}));
   Tensor B = make_tensor(make_gmem_ptr(Bptr), make_shape(n, k), make_stride(k, Int<1>{}));
   Tensor C = make_tensor(make_gmem_ptr(Cptr), make_shape(m, n), make_stride(n, Int<1>{}));
@@ -31,14 +29,18 @@ __global__ void gemm_simple(T *Cptr, const T *Aptr, const T *Bptr, int m, int n,
   // MMA_M = M / (mma_op_m * thr_layout_m)
   // MMA_N = N / (mma_op_n * thr_layout_n)
   // MMA_K = K / (mma_op_k * thr_layout_k)
-  auto tAgA = thr_mma.partition_A(gA); // (MMA, MMA_M, MMA_K, num_tile_k)
-  auto tBgB = thr_mma.partition_B(gB); // (MMA, MMA_N, MMA_K, num_tile_k)
-  auto tCgC = thr_mma.partition_C(gC); // (MMA, MMA_M, MMA_N)
+
+  //  MMA_A = (2, 2, 2)
+  //  MMA_B = (2, 2)
+  //  MMA_C = (2, 2)
+  auto tAgA = thr_mma.partition_A(gA); // (MMA_A, MMA_M, MMA_K, num_tile_k)
+  auto tBgB = thr_mma.partition_B(gB); // (MMA_B, MMA_N, MMA_K, num_tile_k)
+  auto tCgC = thr_mma.partition_C(gC); // (MMA_C, MMA_M, MMA_N)
 
   // register tensor, shape is different with global memory tensor
-  auto tArA = thr_mma.partition_fragment_A(gA(_, _, 0)); // (MMA, MMA_M, MMA_K)
-  auto tBrB = thr_mma.partition_fragment_B(gB(_, _, 0)); // (MMA, MMA_N, MMA_K)
-  auto tCrC = thr_mma.partition_fragment_C(gC(_, _));    // (MMA, MMA_M, MMA_N)
+  auto tArA = thr_mma.partition_fragment_A(gA(_, _, 0)); // (MMA_A, MMA_M, MMA_K)
+  auto tBrB = thr_mma.partition_fragment_B(gB(_, _, 0)); // (MMA_B, MMA_N, MMA_K)
+  auto tCrC = thr_mma.partition_fragment_C(gC(_, _));    // (MMA_C, MMA_M, MMA_N)
   clear(tCrC);
 
 #ifdef PRINT_INFO
@@ -53,12 +55,18 @@ __global__ void gemm_simple(T *Cptr, const T *Aptr, const T *Bptr, int m, int n,
     tCgC : ((_2,_2),_4,_8)
     tCrC : ((_2,_2),_4,_8)
 */
-  if (threadIdx.x == 0 && blockIdx.x == 0 && blockIdx.y == 0)
-  {
+  if (threadIdx.x == 0 && blockIdx.x == 0 && blockIdx.y == 0) {
+    // gA Layout
+    PRINT("gA shape", gA.shape())     
+    PRINT("gA stride", gA.layout())
 
-    PRINT("gA", gA.shape())     
-    PRINT("tAgA", tAgA.shape()) 
-    PRINT("tArA", tArA.shape()) 
+    // tAgA Layout
+    PRINT("tAgA shape", tAgA.shape()) 
+    PRINT("tAgA stride", tAgA.layout())
+
+    // tArA Layout
+    PRINT("tArA shape", tArA.shape()) 
+    PRINT("tArA stride", tArA.layout()) 
 
     PRINT("gB", gB.shape())     
     PRINT("tBgB", tBgB.shape()) 
@@ -72,13 +80,11 @@ __global__ void gemm_simple(T *Cptr, const T *Aptr, const T *Bptr, int m, int n,
 
   int num_tile_k = size<2>(gA);
 #pragma unroll 1
-  for (int itile = 0; itile < num_tile_k; ++itile)
-  {
+  for (int itile = 0; itile < num_tile_k; ++itile) {
     // global memory to register
     // just use cute::copy, not tiled
     copy(tAgA(_, _, _, itile), tArA);
     copy(tBgB(_, _, _, itile), tBrB);
-
     // warp level, use  tiled_mma
     gemm(tiled_mma, tCrC, tArA, tBrB, tCrC);
   }
@@ -86,8 +92,7 @@ __global__ void gemm_simple(T *Cptr, const T *Aptr, const T *Bptr, int m, int n,
   copy(tCrC, tCgC);
 }
 
-int main()
-{
+int main() {
   srand(1000);
 
   using T = cute::half_t;
@@ -100,7 +105,7 @@ int main()
   T *Aptr;
   T *Bptr;
 
-  int m = 1024*64;
+  int m = 1024 * 64;
   int n = 128;
   int k = 1024;
 
@@ -110,21 +115,25 @@ int main()
 
   T *Aptr_host;
   T *Bptr_host;
+  T *Cptr_host;
   Aptr_host = (T *)malloc(sizeof(T) * m * k);
   Bptr_host = (T *)malloc(sizeof(T) * n * k);
-  gen_rand_data(Aptr_host, m * k);
-  gen_rand_data(Bptr_host, n * k);
+  Cptr_host = (T *)malloc(sizeof(T) * m * n);
+  // gen_rand_data(Aptr_host, m * k);
+  // gen_rand_data(Bptr_host, n * k);
+  for (int i = 0; i < m * k; i++) Aptr_host[i] = 1.0;
+  for (int i = 0; i < k * n; i++) Bptr_host[i] = 1.0;
 
   cudaMemcpy(Aptr, Aptr_host, sizeof(T) * m * k, cudaMemcpyHostToDevice);
   cudaMemcpy(Bptr, Bptr_host, sizeof(T) * n * k, cudaMemcpyHostToDevice);
 
-  // M=16,N=8,K=16
+  // MMA 16x8x16xf16
   using mma_op = SM80_16x8x16_F16F16F16F16_TN;
   using mma_traits = MMA_Traits<mma_op>;
   using mma_atom = MMA_Atom<mma_traits>;
   using MMA = decltype(make_tiled_mma(mma_atom{},
-                                      make_layout(Shape<_2, _2, _1>{}),   // the best shape for perf, use 2x2=4 warps = 128 threads
-                                      make_layout(Shape<_1, _1, _1>{}))); // influence little to perf
+                                      make_layout(Shape<_2, _2, _1>{}),   // use 2 x 2 = 4 warps = 128 threads
+                                      make_layout(Shape<_1, _2, _1>{})));
 
   constexpr int kTileM = 128;
   constexpr int kTileN = 128;
@@ -136,8 +145,7 @@ int main()
 
   int count = 100;
   cudaEventRecord(start);
-  for (int i = 0; i < count; ++i)
-  {
+  for (int i = 0; i < count; ++i) {
     gemm_simple<T, kTileM, kTileN, kTileK, MMA><<<grid, block>>>(Cptr, Aptr, Bptr, m, n, k);
   }
   auto err = cudaGetLastError();
@@ -146,13 +154,18 @@ int main()
   cudaEventSynchronize(end);
   cudaEventElapsedTime(&elapsedTime, start, end);
   std::cout << "gemm-simple took " << elapsedTime / count << "ms." << std::endl;
+
+  cudaMemcpy(Cptr_host, Cptr, sizeof(T) * m * n, cudaMemcpyDeviceToHost);
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) std::cout << Cptr_host[i * n + j] << " ";
+    std::cout << std::endl;
+  }
+
 }
 
 template <typename T>
-void gen_rand_data(T *data, int n)
-{
-  for (int i = 0; i < n; ++i)
-  {
+void gen_rand_data(T *data, int n) {
+  for (int i = 0; i < n; i++) {
     float v = (rand() % 200 - 100) * 0.01;
     data[i] = v;
   }
